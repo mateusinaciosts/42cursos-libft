5caaea4a295c9adbba4e7a9056754e86226b55e1
Mateus Santos <matsanto@c3r1p3.42sp.org.br>
2023-05-16 19:22:36 -0300

    new

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: matsanto <matsanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 11:09:40 by matsanto          #+#    #+#             */
-/*   Updated: 2023/05/16 19:07:12 by matsanto         ###   ########.fr       */
+/*   Updated: 2023/05/16 19:12:31 by matsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,6 @@ int		ft_toupper(int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char    *ft_itoa(int n);
+char	*ft_itoa(int n);
 
 #endif
