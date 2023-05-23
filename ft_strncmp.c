39ff9f5dc933e612dd9464b62a05e7bc4990b19e
Mateus Santos <matsanto@c1r3p3.42sp.org.br>
2023-05-23 17:01:52 -0300

    testando strnstr

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: matsanto <matsanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:38:47 by matsanto          #+#    #+#             */
-/*   Updated: 2023/05/16 10:00:32 by matsanto         ###   ########.fr       */
+/*   Updated: 2023/05/23 16:20:09 by matsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	unsigned long int	index;
 
-	i = 0;
-	if (!*s1 && !*s2)
-		return (0);
-	if (n == 0)
-		return (0);
-	while ((s1[i] && s2[i]) && (i < n - 1))
+	index = 0;
+	while (index < n && (s1[index] != '\0' || s2[index] != '\0'))
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		if (s1[index] != s2[index])
+			return ((unsigned char)s1[index] - (unsigned char)s2[index]);
+		index++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,16 +6,18 @@
 /*   By: matsanto <matsanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 10:49:45 by matsanto          #+#    #+#             */
-/*   Updated: 2023/05/16 09:59:37 by matsanto         ###   ########.fr       */
+/*   Updated: 2023/05/23 16:59:05 by matsanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-char	*ft_strnstr(const char *dest, const char *src, size_t n)
+char *ft_strnstr(const char *dest, const char *src, size_t n)
 {
-	size_t	i;
-	size_t	j;
+	size_t i;
+	size_t j;
 
 	i = 0;
 	j = 0;
@@ -31,5 +33,73 @@ char	*ft_strnstr(const char *dest, const char *src, size_t n)
 			return ((char *)&dest[i]);
 		i++;
 	}
+	return (0);
+}
+
+void ft_print_result(char const *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	write(1, s, len);
+}
+
+int main()
+{
+	const char *str;
+	if (ft_strnstr("lorem ipsum dolor sit amet", "lorem", 15) == "lorem ipsum dolor sit amet")
+		printf("SUCESSO 01\n");
+	else
+		ft_print_result(str);
+
+	if (ft_strnstr("lorem ipsum dolor sit amet", "ipsum", 15) == "ipsum dolor sit amet")
+		printf("SUCESSO 02\n");
+	else
+		ft_print_result(str);
+
+	if (ft_strnstr("lorem ipsum dolor sit lorem ipsum dolor", "ipsum", 35) == "ipsum dolor sit lorem ipsum dolor")
+		printf("SUCESSO 03\n");
+	else
+		ft_print_result(str);
+
+	if (ft_strnstr("lorem ipsum dolor sit amet", "", 10) == "lorem ipsum dolor sit amet")
+		printf("SUCESSO 04\n");
+	else
+		ft_print_result(str);
+
+	if (ft_strnstr("lorem ipsum dolor sit amet", "ipsumm", 30) == NULL)
+		printf("SUCESSO 05\n");
+	else
+	{
+		char *str = ft_strnstr("lorem ipsum dolor sit amet", "ipsumm", 30);
+		printf("%s\n", str);
+	}
+	if (ft_strnstr("lorem ipsum dolor sit amet", "dol", 30) == "dolor sit amet")
+		printf("SUCESSO 06\n");
+	else
+		ft_print_result(str);
+
+	if (ft_strnstr("lorem ipsum dolor sit amet", "consectetur", 30) == NULL)
+		printf("SUCESSO 07\n");
+	else
+		ft_print_result(str);
+
+	if (ft_strnstr("lorem ipsum dolor sit amet", "sit", 10) == NULL)
+		printf("SUCESSO 08\n");
+	else
+		ft_print_result(str);
+
+	if (ft_strnstr("lorem ipsum dolor sit amet", "dolor", 15) == NULL)
+		printf("SUCESSO 09\n");
+	else
+		ft_print_result(str);
+
+	if (ft_strnstr("lorem ipsum dolor sit amet", "dolor", 0) == NULL)
+		printf("SUCESSO 10\n");
+	else
+		ft_print_result(str);
+
 	return (0);
 }
